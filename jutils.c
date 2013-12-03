30dbaf8cec8f81e8a8c784649f60b3817cc2779d
Ragesh Radhakrishnan <ragesh.r@linaro.org>
2013-12-03 15:29:14 +0530

    Add jmin routine
    Jmin routine to find min of 2 long type inputs

--- jutils.c
+++ jutils.c
@@ -86,7 +86,11 @@ jround_up (long a, long b)
   return a - (a % b);
 }
 
-
+GLOBAL(long)
+jmin (long a, long b)
+{
+  return a < b ? a : b;
+}
 /* On normal machines we can apply MEMCOPY() and MEMZERO() to sample arrays
  * and coefficient-block arrays.  This won't work on 80x86 because the arrays
  * are FAR and we're assuming a small-pointer memory model.  However, some
